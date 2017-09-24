54ac978eb16215a2c53b8499cf5d5d1e508f14fe
Pranit Jha <pranit.jha@acquia.com>
2017-09-24 12:53:48 +0530

    Day 12 exercise done

---
+++ day12/modules/custom/mymodule/src/Form/ContactDeleteForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\mymodule\Form;
+
+use Drupal\Core\Entity\ContentEntityConfirmFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Provides a form for deleting a mymodule entity.
+ *
+ * @ingroup mymodule
+ */
+class ContactDeleteForm extends ContentEntityConfirmFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return $this->t('Are you sure you want to delete entity %name?', ['%name' => $this->entity->label()]);
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * If the delete command is canceled, return to the contact list.
+   */
+  public function getCancelUrl() {
+    return new Url('entity.mymodule_contact.collection');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Delete the entity and log the event. logger() replaces the watchdog.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $entity = $this->getEntity();
+    $entity->delete();
+
+    $this->logger('mymodule')->notice('@type: deleted %title.',
+      [
+        '@type' => $this->entity->bundle(),
+        '%title' => $this->entity->label(),
+      ]);
+    $form_state->setRedirect('entity.mymodule_contact.collection');
+  }
+
+}
